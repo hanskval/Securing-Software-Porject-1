7eea3aefa593d5266d961336dbba2bb6a6fd7d84
hanskval <valtteri.hanska@helsinki.fi>
2026-08-19 15:54:45 +0300

    updated Cryptographic Failures to code be first vulnerable

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
 secret_key = "18fd24bf6a2ad4dac04a33963db1c42f"
+
+
+"""
+# Fix: secret key is read from .env file via environment variable
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+secret_key = os.environ.get("SECRET_KEY")
+if not secret_key:
+    raise RuntimeError("SECRET_KEY missing .env-file") 
+"""
